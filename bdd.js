e53b19e68a366192d70b2bb159f84b31cddd209a
TommyV86 <76942786+TommyV86@users.noreply.github.com>
2021-10-06 12:27:25 +0200

    Update bdd.js

--- bdd.js
+++ bdd.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb')
 
 const dbURI =
-  "mongodb+srv://MLS:mlsproject@projetmls.ohrb3.mongodb.net/test?authSource=admin&replicaSet=atlas-ghh06l-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass&retryWrites=true&ssl=true";
+  "";
 
 module.exports = { MongoClient, dbURI }
